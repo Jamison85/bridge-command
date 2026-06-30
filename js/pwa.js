ba85313167bbc81f1adcd124cb49f82a72b617d2
Jamison85 <jamisonr85@gmail.com>
2026-06-30 15:20:51 -0500

    Use visible walk completion flow

--- js/pwa.js
+++ js/pwa.js
@@ -5,7 +5,7 @@ import "./final-task-highlight.js";
 import "./review-layer-variety.js";
 import "./runtime-clean-bridge.js?v=1";
 import "./shift-brain.js";
-import "./walk-fix.js";
+import "./walk-flow-v2.js?v=1";
 
 const installHintKey = "storePilot.installHintSeen.v1";
 let deferredInstallPrompt = null;
