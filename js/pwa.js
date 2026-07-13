e76f36957500ba2a81becb97792dcdc0b049c68b
Jamison85 <jamisonr85@gmail.com>
2026-07-13 02:18:46 -0500

    Bump PWA cache for Proof of Work Pack

--- js/pwa.js
+++ js/pwa.js
@@ -1,5 +1,5 @@
 const installHintKey = "storePilot.installHintSeen.v1";
-const reloadGuardKey = "storePilot.pwaReloaded.command-center-15";
+const reloadGuardKey = "storePilot.pwaReloaded.command-center-16";
 let deferredInstallPrompt = null;
 let refreshingForUpdate = false;
 
@@ -46,7 +46,7 @@ function activateWaitingWorker(registration) {
 
 async function registerServiceWorker() {
   try {
-    const registration = await navigator.serviceWorker.register("./sw.js?v=command-center-15", { updateViaCache: "none" });
+    const registration = await navigator.serviceWorker.register("./sw.js?v=command-center-16", { updateViaCache: "none" });
     await registration.update();
     activateWaitingWorker(registration);
 
