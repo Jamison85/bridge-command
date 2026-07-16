b608e0732f95545b4497bae0929f3bf716f0d2b9
Jamison85 <jamisonr85@gmail.com>
2026-07-16 09:44:20 -0500

    Bump PWA cache for Loretta Win

--- js/pwa.js
+++ js/pwa.js
@@ -1,5 +1,5 @@
 const installHintKey = "storePilot.installHintSeen.v1";
-const reloadGuardKey = "storePilot.pwaReloaded.command-center-22";
+const reloadGuardKey = "storePilot.pwaReloaded.command-center-23";
 let deferredInstallPrompt = null;
 let refreshingForUpdate = false;
 
@@ -46,7 +46,7 @@ function activateWaitingWorker(registration) {
 
 async function registerServiceWorker() {
   try {
-    const registration = await navigator.serviceWorker.register("./sw.js?v=command-center-22", { updateViaCache: "none" });
+    const registration = await navigator.serviceWorker.register("./sw.js?v=command-center-23", { updateViaCache: "none" });
     await registration.update();
     activateWaitingWorker(registration);
 
